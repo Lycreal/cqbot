56bf501f6add62d6e3b4358aab6a4fef0f35a0d0
Lycreal <jgsly123@gmail.com>
2019-09-10 22:29:40 +0800

    更改群发模式，增加时间间隔检测

--- plugins/monitor_bili/__init__.py
+++ plugins/monitor_bili/__init__.py
@@ -3,9 +3,10 @@
 import urllib.request
 import json
 import nonebot
+import time
 from datetime import datetime, timezone, timedelta
-from aiocqhttp.exceptions import Error as CQHttpError
-from config import GROUP_TST, channelList_bili as channelList
+from config import channelList_bili as channelList
+from utils_bot.msg_ops import send_to_superusers, send_to_groups
 
 __plugin_name__ = '监控器_bili'
 __plugin_usage__ = r'''feature: 监控器_bili
@@ -16,10 +17,10 @@ bot = nonebot.get_bot()
 
 
 class Channel:
-    last_check: str = '0000-00-00 00:00:00'
     live_status: int = 0
-    live_time: str = '0000-00-00 00:00:00'
-    _last_live: str = '0000-00-00 00:00:00'
+    live_time: str = '1970-01-02 00:00:00'  # 本次开播时间
+    last_live: str = '1970-01-02 00:00:00'  # 上次开播时间
+    last_check: str = '1970-01-02 00:00:00'  # 上次检测到直播时间
 
     def __init__(self, room_id, name):
         self.room_id: str = room_id  # 直播间房间号
@@ -27,15 +28,23 @@ class Channel:
         self.live_url: str = f'https://api.live.bilibili.com/room/v1/Room/get_info?id={room_id}'
 
     def update(self):
-        self.last_check = datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')
         json_s = urllib.request.urlopen(self.live_url).read().decode('utf-8')
         json_d = json.loads(json_s)
         self.live_status = json_d.get('data').get('live_status')
-        self.live_time: str = json_d.get('data').get('live_time')
-        if self.live_time != self._last_live:
-            self._last_live = self.live_time
+        self.live_time = json_d.get('data').get('live_time')
+        if self.live_status == 1:
+            self.last_check = datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')  # 当前时间
+        if self.time_delta(self.live_time, self.last_live) > 600:
+            self.last_live = self.live_time
             return 1
         return 0
+
+    @staticmethod
+    def time_delta(ta, tb):
+        a = time.strptime(ta, "%Y-%m-%d %H:%M:%S")
+        b = time.strptime(tb, "%Y-%m-%d %H:%M:%S")
+        delta = time.mktime(a) - time.mktime(b)
+        return int(delta)
 
     def __str__(self):
         msg = f'Channel Name: {self.name}\n'
@@ -56,14 +65,14 @@ channels = [Channel(room_id, name) for room_id, name in channelList]
 v = circle(len(channels))
 
 
-@nonebot.scheduler.scheduled_job('interval', seconds=5)
+@nonebot.scheduler.scheduled_job('interval', seconds=3)
 async def _():
-    channel = channels[next(v)]
-    if channel.update():
-        if channel.live_status == 1:
-            msg = f'{channel.name}于{channel.live_time[11:16]}开播了: '
-            msg += f'https://live.bilibili.com/{channel.room_id}'
-            try:
-                await bot.send_group_msg(group_id=GROUP_TST, message=msg)
-            except CQHttpError:
-                pass
+    try:
+        channel = channels[next(v)]
+        if channel.update():
+            if channel.live_status == 1:
+                msg = f'{channel.name}于{channel.live_time[11:16]}开播了: '
+                msg += f'https://live.bilibili.com/{channel.room_id}'
+                await send_to_groups(msg)
+    except Exception as e:
+        await send_to_superusers(msg=repr(e))
--- utils_bot/msg_ops.py
+++ utils_bot/msg_ops.py
@@ -1,4 +1,5 @@
 from nonebot import get_bot
+from config import GROUPS
 
 bot = get_bot()
 SUPERUSERS = bot.config.SUPERUSERS
@@ -8,6 +9,11 @@ MY_NAMES = bot.config.NICKNAME.union({'机器人', '复读机'})
 async def send_to_superusers(msg: str):
     for eachId in SUPERUSERS:
         await bot.send_private_msg(user_id=eachId, message=msg)
+
+
+async def send_to_groups(msg: str):
+    for groupId in GROUPS:
+        await bot.send_group_msg(group_id=groupId, message=msg)
 
 
 def msg_is_calling_me(msg: str) -> bool:
