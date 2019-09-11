b9430e780c9f5e030207ac435f23915807986177
Lycreal <jgsly123@gmail.com>
2019-09-11 13:16:25 +0800

    变更判定方式

--- plugins/monitor_bili/__init__.py
+++ plugins/monitor_bili/__init__.py
@@ -14,15 +14,14 @@ __plugin_usage__ = r'''feature: 监控器_bili
 
 bot = nonebot.get_bot()
 
-time_pre = timedelta(minutes=10)
+time_pre = timedelta(minutes=5)
 
 
 class Channel:
     live_status: int = 0
-    live_time: str = '1970-01-02 00:00:00'  # 本次开播时间
-    last_live: str = '1970-01-02 00:00:00'  # 上次开播时间
-
-    # last_check: str = '1970-01-02 00:00:00'  # 上次检测到直播时间
+    live_time: str = '0000-00-00 00:00:00'  # 本次开播时间
+    last_live: str = '1970-01-02 00:00:00'  # 上次下播时间
+    last_check: str = '1970-01-02 00:00:00'  # 上次检测到直播时间
 
     def __init__(self, room_id, name):
         self.room_id: str = room_id  # 直播间房间号
@@ -31,20 +30,27 @@ class Channel:
         self.last_live = (datetime.now(timezone(timedelta(hours=8))) - time_pre).strftime('%Y-%m-%d %H:%M:%S')
 
     def update(self):
+        # 获取信息
         json_s = urllib.request.urlopen(self.live_url).read().decode('utf-8')
         json_d = json.loads(json_s)
         self.live_status = json_d.get('data').get('live_status')
         self.live_time = json_d.get('data').get('live_time')
-        # if self.live_status == 1:
-        #     self.last_check = datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')  # 当前时间
+        # 开播状态
+        if self.live_status == 1:
+            self.last_check = datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')  # 当前时间
+        # 距离上次下播大于time_pre
         if self.time_delta(self.live_time, self.last_live) > time_pre:
-            self.last_live = self.live_time
+            if self.live_status == 1:
+                self.last_live = self.last_check
             return 1
         return 0
 
     @staticmethod
     def time_delta(ta, tb):
-        a: datetime = datetime.strptime(ta, "%Y-%m-%d %H:%M:%S")
+        if ta == '0000-00-00 00:00:00':
+            a = datetime.fromtimestamp(0)
+        else:
+            a: datetime = datetime.strptime(ta, "%Y-%m-%d %H:%M:%S")
         b: datetime = datetime.strptime(tb, "%Y-%m-%d %H:%M:%S")
         delta: timedelta = a - b
         return delta
@@ -70,12 +76,9 @@ v = circle(len(channels))
 
 @nonebot.scheduler.scheduled_job('interval', seconds=3)
 async def _():
-    try:
-        channel = channels[next(v)]
-        if channel.update():
-            if channel.live_status == 1:
-                msg = f'{channel.name}于{channel.live_time[11:16]}开播了: '
-                msg += f'https://live.bilibili.com/{channel.room_id}'
-                await send_to_groups(msg)
-    except Exception as e:
-        await send_to_superusers(msg=repr(e))
+    channel = channels[next(v)]
+    if channel.update():
+        if channel.live_status == 1:
+            msg = f'{channel.name}于{channel.live_time[11:16]}开播了: '
+            msg += f'https://live.bilibili.com/{channel.room_id}'
+            await send_to_groups(msg)
