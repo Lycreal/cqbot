11b2ad0940404b0cb1f402a8080f200bcea5a4da
Lycreal <jgsly123@gmail.com>
2019-09-11 13:35:02 +0800

    项目结构调整

---
+++ config_bot.py
@@ -0,0 +1,11 @@
+from nonebot.default_config import *
+
+SUPERUSERS = {}
+NICKNAME = {'机器人', '复读机'}
+COMMAND_START = {'.'}
+SESSION_EXPIRE_TIMEOUT = timedelta(minutes=1)
+
+HOST = '172.17.0.1'
+PORT = 8080
+
+GROUPS = []
