053de2c188426f73c7b57e072a1e462167961847
Lycreal <jgsly123@gmail.com>
2019-09-20 18:38:33 +0800

    新增Class Monitor，修复title获取
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- plugins/live_monitor/__init__.py
+++ plugins/live_monitor/__init__.py
@@ -1,9 +1,10 @@
 __all__ = ['bili', 'cc', 'general', 'youtube']
 
-from .general import Channel as GeneralChannel
-from .youtube import YoutubeChannel
-from .bili import BiliChannel
-from .cc import NetEaseChannel
+from plugins.live_monitor.general import Channel as GeneralChannel
+from plugins.live_monitor.youtube import YoutubeChannel
+from plugins.live_monitor.bili import BiliChannel
+from plugins.live_monitor.cc import NetEaseChannel
+import time
 
 
 def init_channel(type: str, id: str, name: str):
@@ -15,3 +16,52 @@ def init_channel(type: str, id: str, name: str):
         return NetEaseChannel(id, name)
     else:
         return KeyError(type)
+
+
+class Monitor:
+    channel_list: list = []
+    debug = False
+    pos = -1
+    notify = print
+
+    def __init__(self, channel_type, debug=False):
+        self.channel_type = channel_type
+        self.debug = debug
+
+    def add(self, ch_list):
+        for ch_info in ch_list:
+            ch = init_channel(self.channel_type, *ch_info)
+            if ch.id not in [ch.id for ch in self.channel_list]:
+                self.channel_list.append(ch)
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def next(self):
+        if self.channel_list:
+            self.pos = self.pos + 1 if self.pos < len(self.channel_list) - 1 else 0
+            return self.channel_list[self.pos]
+        else:
+            return None
+
+    def register_notifier(self, notify=print):
+        self.notify = notify
+
+    def run(self):
+        channel: GeneralChannel = self.next()
+        print(time.strftime('%H:%M:%S', time.localtime()))
+        if channel.update() or self.debug:
+            self.notify(channel)
+            self.notify(channel.notify())
+
+
+if __name__ == '__main__':
+    monitor = Monitor('you', debug=True)
+    monitor.add([('UCdn5BQ06XqgXoAxIhbqw5Rg', 'FBK')])
+    monitor.add([('UCveZ9Ic1VtcXbsyaBgxPMvg', 'meiji')])
+    while True:
+        monitor.run()
+        time.sleep(10)
--- plugins/live_monitor/youtube.py
+++ plugins/live_monitor/youtube.py
@@ -1,8 +1,9 @@
 import re
 from plugins.live_monitor.general import Channel
 
-
-# import requests
+import requests
+import ast
+import json
 
 
 class YoutubeChannel(Channel):
@@ -11,9 +12,6 @@ class YoutubeChannel(Channel):
         self.live_url = self.api_url
 
     def resolve(self, html_s):
-        videoId = re.search(r'<meta itemprop="videoId" content="([^"]*)">', html_s).group(1)
-        self.live_url = f'https://www.youtube.com/watch?v={videoId}'
-
         content = re.search(r'.*RELATED_PLAYER_ARGS.*', html_s).group()
         with open(self.name, 'w', encoding='utf8') as f:
             f.write(content)
@@ -23,13 +21,22 @@ class YoutubeChannel(Channel):
             self.live_status = '2'
         elif re.search(r'Started streaming', content):
             self.live_status = '1'
-            self.title = re.search(r'\\"videoTitle\\":\\"([^\\]*)\\"', content).group(1)
         else:
             raise KeyError('找不到关键字')
+        self.get_title(content)
 
-    # def get_status(self):
-    #     html_s = requests.get(self.api_url, proxies={'https': 'socks5://127.0.0.1:10808'}).text
-    #     self.resolve(html_s)
+    def get_status(self):
+        html_s = requests.get(self.api_url, proxies={'https': 'socks5://127.0.0.1:10808'}).text
+        self.resolve(html_s)
+
+    def get_title(self, content):
+        match = re.search(r'\\"shareVideoEndpoint\\":{(.*?)}},', content)
+        if match:
+            text = '{%s}' % match.group(1)
+            text_f = ast.literal_eval("'{}'".format(text))
+            shareVideoEndpoint = json.loads(text_f)
+            self.title = shareVideoEndpoint['videoTitle']
+            self.live_url = shareVideoEndpoint['videoShareUrl']
 
 
 def main():
