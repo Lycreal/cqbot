a5f6cc74cd852db900817fa7818a700cf6555f30
Lycreal <jgsly123@gmail.com>
2019-09-22 17:38:09 +0800

    update help
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- plugins/monitor_notify/__init__.py
+++ plugins/monitor_notify/__init__.py
@@ -7,3 +7,8 @@ import plugins.monitor_notify.youtube
 import plugins.monitor_notify.cc
 
 # GROUPS = [GROUP_TST, GROUP_BTR, GROUP_KR]
+
+__plugin_name__ = '直播监控'
+__plugin_usage__ = r'''feature: 直播监控
+查看状态：monitor_bili_status
+'''
