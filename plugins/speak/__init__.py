4b6631729304bdb7ae2d2f2961e8f139b69dd5b8
Lycreal <jgsly123@gmail.com>
2019-09-22 22:59:37 +0800

    操纵发言
    Signed-off-by: Lycreal <jgsly123@gmail.com>

---
+++ plugins/speak/__init__.py
@@ -0,0 +1,27 @@
+import nonebot
+from nonebot import on_command, CommandSession
+from nonebot.default_config import SUPERUSERS
+
+bot: nonebot.NoneBot = nonebot.get_bot()
+
+
+@on_command('speak', aliases=('say', 'echo'), only_to_me=True, permission=SUPERUSERS)
+async def speak(session: CommandSession):
+    group = session.get('group')
+    word = session.get('word')
+    await bot.send_group_msg(group_id=group, message=word)
+
+
+@speak.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    group: str
+    word: str
+    group, word = stripped_arg.split(maxsplit=1)
+    if group.isdecimal():
+        session.state['group'] = group
+        session.state['word'] = word
+    else:
+        session.state['group'] = session.ctx['group_id']
+        session.state['word'] = stripped_arg
+    return
