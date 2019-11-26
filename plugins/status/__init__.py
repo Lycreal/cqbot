68bb63b60f692b7bc6bc8b30afa2f82740096fd6
Lycreal <jgsly123@gmail.com>
2019-11-26 13:58:03 +0800

    uptime去除毫秒显示
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- plugins/status/__init__.py
+++ plugins/status/__init__.py
@@ -1,18 +1,17 @@
-from datetime import datetime, timedelta as ori_timedelta
+from datetime import datetime, timedelta
 from nonebot import get_bot, on_command, CommandSession
 
 
-class timedelta(ori_timedelta):
-    def __str__(self):
-        mm, ss = divmod(self.seconds, 60)
-        hh, mm = divmod(mm, 60)
-        s = "%d:%02d:%02d" % (hh, mm, ss)
-        if self.days:
-            def plural(n):
-                return n, abs(n) != 1 and "s" or ""
+def timedelta2str(td):
+    mm, ss = divmod(td.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    s = "%d:%02d:%02d" % (hh, mm, ss)
+    if td.days:
+        def plural(n):
+            return n, abs(n) != 1 and "s" or ""
 
-            s = ("%d day%s, " % plural(self.days)) + s
-        return s
+        s = ("%d day%s, " % plural(td.days)) + s
+    return s
 
 
 bot = get_bot()
@@ -23,5 +22,5 @@ start_time = datetime.now()
 async def _(session: CommandSession):
     now = datetime.now()
     uptime: timedelta = now - start_time
-    msg = f'uptime: {uptime}'
+    msg = f'uptime: {timedelta2str(uptime)}'
     await session.send(msg)
