bc8cfa77995151cf6fa215a86af54a89c977478e
Lycreal <jgsly123@gmail.com>
2019-12-12 20:25:48 +0800

    修正数据位置
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import nonebot
 from nonebot.log import logger
 import config_bot
 
+root_path = path.dirname(__file__)
 if __name__ == '__main__':
     nonebot.init(config_bot)
     logger.setLevel(logging.INFO)
-    root_path = path.dirname(__file__)
     nonebot.load_plugins(
         path.join(root_path, 'plugins'),
         'plugins'
