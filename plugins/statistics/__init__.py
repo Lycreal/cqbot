b28ea8ffa83814f1e96fbc8516b4a50892c5848b
Lycreal <jgsly123@gmail.com>
2019-12-15 15:42:13 +0800

    再使用pydantic
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- plugins/statistics/__init__.py
+++ plugins/statistics/__init__.py
@@ -1,4 +1,5 @@
 import pickle
+from pydantic.dataclasses import dataclass
 from typing import Dict, List, Any
 from datetime import datetime
 import nonebot
@@ -7,30 +8,30 @@ from config_bot import data_path
 
 
 class Statistics:
-    loaded = False
-    data_count: Dict[int, Dict[int, int]] = {}  # 计数用字典
-    data_time: Dict[int, List[datetime]] = {}  # 计时用字典
-    data_name: Dict[int, Dict[int, str]] = {}  # 保存名字的字典
-    last_day_msg_count: Dict[int, str] = {}  # 昨日计数
-    last_day_msg_time: Dict[int, str] = {}  # 昨日计时
+    @dataclass
+    class SaveData:
+        count: Dict[int, Dict[int, int]] = {}  # 计数用字典
+        time: Dict[int, List[datetime]] = {}  # 计时用字典
+        name: Dict[int, Dict[int, str]] = {}  # 保存名字的字典
+        last_day_msg_count: Dict[int, str] = {}  # 昨日计数
+        last_day_msg_time: Dict[int, str] = {}  # 昨日计时
 
-    last_save_time = None
+    loaded = False
+    data = SaveData()
     save_file = data_path.joinpath('statistics.dat')
 
     @classmethod
     def load(cls):
         try:
             with cls.save_file.open('rb') as f:
-                save_data = pickle.load(f)
-            cls.data_count, cls.data_time, cls.data_name, cls.last_day_msg_count, cls.last_day_msg_time = save_data
+                cls.data = pickle.load(f)
         except FileNotFoundError:
             cls.save()
 
     @classmethod
     def save(cls):
-        save_data = cls.data_count, cls.data_time, cls.data_name, cls.last_day_msg_count, cls.last_day_msg_time
         with cls.save_file.open('wb') as f:
-            pickle.dump(save_data, f)
+            pickle.dump(cls.data, f)
 
     @classmethod
     def incoming_msg(cls, group_id: int, user_id: int, name: str):
@@ -38,47 +39,47 @@ class Statistics:
             cls.load()
             cls.loaded = True
         cls.init(group_id, user_id, name)
-        cls.data_count[group_id][user_id] += 1
-        cls.data_time[group_id].append(datetime.now())
+        cls.data.count[group_id][user_id] += 1
+        cls.data.time[group_id].append(datetime.now())
         cls.save()
 
     @classmethod
     def init(cls, group_id: int, user_id: int, name: str):
         # 初始化group_id和user_id结构
-        if group_id not in cls.data_time.keys():
-            cls.data_time.update({group_id: []})
+        if group_id not in cls.data.time.keys():
+            cls.data.time.update({group_id: []})
 
-        if group_id not in cls.data_count.keys():
-            cls.data_count.update({group_id: {}})
-        if user_id not in cls.data_count[group_id].keys() and user_id:
-            cls.data_count[group_id].update({user_id: 0})
+        if group_id not in cls.data.count.keys():
+            cls.data.count.update({group_id: {}})
+        if user_id not in cls.data.count[group_id].keys() and user_id:
+            cls.data.count[group_id].update({user_id: 0})
 
-        if group_id not in cls.data_name.keys():
-            cls.data_name.update({group_id: {}})
-        cls.data_name[group_id].update({user_id: name})
+        if group_id not in cls.data.name.keys():
+            cls.data.name.update({group_id: {}})
+        cls.data.name[group_id].update({user_id: name})
 
     @classmethod
     def clear(cls):
-        for group_id in cls.data_count.keys():
-            cls.last_day_msg_count.update({group_id: cls.top_talker(group_id)})
-        for group_id in cls.data_time.keys():
-            cls.last_day_msg_time.update({group_id: cls.time_result(group_id)})
-        cls.data_count = {}
-        cls.data_time = {}
+        for group_id in cls.data.count.keys():
+            cls.data.last_day_msg_count.update({group_id: cls.top_talker(group_id)})
+        for group_id in cls.data.time.keys():
+            cls.data.last_day_msg_time.update({group_id: cls.time_result(group_id)})
+        cls.data.count = {}
+        cls.data.time = {}
 
     @classmethod
     def top_talker(cls, group_id: int, number=5):  # <20
-        listed = cls.sort_dict(cls.data_count[group_id])
+        listed = cls.sort_dict(cls.data.count[group_id])
         msg = f'今日发言Top{number}:'
         for i in range(number if len(listed) > number else len(listed)):
             user_id = listed[i][0]
             count = listed[i][1]
-            msg += f'\n{cls.data_name[group_id][user_id]}:{count}'
+            msg += f'\n{cls.data.name[group_id][user_id]}:{count}'
         return msg
 
     @classmethod
     def time_result(cls, group_id: int):
-        result = [time.hour - 4 if time.hour >= 4 else time.hour + 24 - 4 for time in cls.data_time[group_id]]
+        result = [time.hour - 4 if time.hour >= 4 else time.hour + 24 - 4 for time in cls.data.time[group_id]]
         a = [0] * 12
         for i in range(12):
             a[i] += result.count(0 + 2 * i)
@@ -141,14 +142,14 @@ async def _(session: CommandSession):
 @on_command('stat3', only_to_me=False)
 async def _(session: CommandSession):
     group_id: int = session.ctx['group_id']
-    msg = Statistics.last_day_msg_count[group_id].replace('今日', '昨日')
+    msg = Statistics.data.last_day_msg_count[group_id].replace('今日', '昨日')
     await session.send(msg)
 
 
 @on_command('stat4', only_to_me=False)
 async def _(session: CommandSession):
     group_id: int = session.ctx['group_id']
-    msg = Statistics.last_day_msg_time[group_id].replace('今日', '昨日')
+    msg = Statistics.data.last_day_msg_time[group_id].replace('今日', '昨日')
     await session.send(msg)
 
 
