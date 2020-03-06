d1f7590eafdf69201cb185fa5d6c9d9f6870936b
Lycreal <jgsly123@gmail.com>
2020-03-06 13:49:54 +0800

    整理项目结构
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     nonebot.init(config_bot)
     logger.setLevel(logging.INFO)
     for dir_ in config_bot.root_path.joinpath('plugins').iterdir():
-        if dir_.is_dir():
+        if dir_.is_dir() and dir_.name != 'disabled':
             nonebot.load_plugins(str(dir_), f'plugins.{dir_.name}')
     logger.info('Starting')
     nonebot.run()
