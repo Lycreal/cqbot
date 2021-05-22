0e967f7469c79ee26026472312552955349b6e49
Lycreal <jgsly123@gmail.com>
2021-05-22 17:33:26 +0800

    test: refactor cat

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -6,6 +6,8 @@ from .utils import websocket
 
 @pytest.mark.timeout(40)
 def test_cat(websocket):
+    # doesn't work for github actions, reason unknown
+
     websocket.send_json(generate_group_message('猫猫'))
     respond = websocket.receive_json()
 
@@ -13,14 +15,3 @@ def test_cat(websocket):
 
     assert respond['action'] in ['send_msg', 'send_private_msg', 'send_group_msg']
     assert any(message['type'] in ['text', 'image'] for message in respond['params']['message'])
-
-
-# @pytest.mark.timeout(20)
-# def test_search_pic(websocket):
-#     msg = f'搜图 [CQ:image,url=https://pixiv.cat/89937761.jpg]'
-#     websocket.send_json(generate_group_message(msg))
-#
-#     respond = websocket.receive_json()
-#     assert respond['action'] in ['send_msg', 'send_private_msg', 'send_group_msg']
-#     assert any('pixiv' in message['data']['text']
-#                for message in respond['params']['message'] if message['type'] == 'text')
