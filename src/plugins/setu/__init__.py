161a86bf9c83d6f3743c4cb1e6c59b6335658f41
Lycreal <jgsly123@gmail.com>
2021-06-06 19:25:33 +0800

    chore: fix setu timeout judgement

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,17 +1,20 @@
 import asyncio
 import random
 import re
+import traceback
 from base64 import b64encode
+from typing import List, Optional
 
 from nonebot import Bot, on_message, logger
 from nonebot.adapters import Event
 from nonebot.adapters.cqhttp import MessageEvent, MessageSegment, GroupMessageEvent
 from nonebot.plugin import require
 from nonebot.typing import T_State
+from numpy import bincount
 
 from .config import plugin_config
 from .datasource import SetuResp
-from .exceptions import TimeoutException
+from .exception import TimeoutException
 from .model import SetuData, SetuDatabase
 from .utils import CoolDown, shuzi2number, shuffle
 
@@ -67,33 +70,36 @@ async def sendSetu(bot: Bot, event: MessageEvent, state: T_State) -> None:
     data_array = resp.data
     number = min(state['number_of_setu'], len(data_array))
 
-    # 延时逐个启动任务
-    num_exception = 0
-    num_timeout = 0
-    for i, setu_data in enumerate(random.sample(data_array, k=number)):
-        try:
-            image_bytes = await setu_data.get()
+    async def send(setu_data_: SetuData) -> None:
+        image_bytes = await setu_data_.get()
+        if isinstance(event, GroupMessageEvent):
+            image_bytes = shuffle(image_bytes)
+        file = f"base64://{b64encode(image_bytes).decode()}"
 
-            if isinstance(event, GroupMessageEvent):
-                image_bytes = shuffle(image_bytes)
-
-            file = f"base64://{b64encode(image_bytes).decode()}"
-            logger.info(f'发送色图: {setu_data.url}')
+        async with lock:
+            logger.info(f'发送色图: {setu_data_.url}')
             ret = await bot.send(event, MessageSegment.image(file))
+            cd.update(event.sender.user_id)
 
             if isinstance(event, GroupMessageEvent):
                 message_id = ret['message_id']
                 asyncio.create_task(check_and_recall(bot, message_id, image=image_bytes))
 
-            cd.update(event.sender.user_id)
-        except TimeoutException as e:
-            import traceback
-            logger.error('\n'.join(traceback.format_exception(type(e), e, e.__traceback__)))
-            num_timeout += 1
-        except Exception as e:
-            import traceback
-            logger.error('\n'.join(traceback.format_exception(type(e), e, e.__traceback__)))
-            num_exception += 1
+    # 延时逐个启动任务
+    tasks: List[asyncio.Task[None]] = []
+    lock = asyncio.Lock()
+    for i, setu_data in enumerate(random.sample(data_array, k=number)):
+        task = asyncio.create_task(send(setu_data))
+        tasks.append(task)
+
+    # 等待返回
+    done, pending = await asyncio.wait(tasks, timeout=30)
+    exceptions: List[Optional[BaseException]] = [task.exception() for task in done if task.exception()]
+    num_exception, num_timeout = bincount([isinstance(exc, TimeoutException) for exc in exceptions], minlength=2).astype(object)
+    num_timeout += len([t.cancel() for t in pending])
+
+    for exc in exceptions:
+        traceback.print_exception(type(exc), exc, exc.__traceback__, limit=5)
 
     # 报告结果
     if num_exception or num_timeout:
